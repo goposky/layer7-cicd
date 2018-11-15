f49d218ea6a09ddb321c5a596f781d6e0f2accd8
amreshh <amresh.hanoeman@devoteam.com>
2018-11-15 09:06:41 +0100

    adjustments

--- scripts/temp.py
+++ scripts/temp.py
@@ -3,163 +3,218 @@
 
 import xml.etree.ElementTree as ET
 import requests
+import os
+import pathlib
+import subprocess
 
 
-# Get the buildingblock policies based on their folder id
-def getBuildingBlocks(xmlContents, namespaces, folderId):
-    tree = ET.parse(xmlContents)
-    root = tree.getroot()
+class Gateway:
+    def __init__(self, gateway, argFile, username, password, restmanUrl, namespaces):
+        self.gateway = gateway
+        self.argFile = argFile
+        self.username = username
+        self.password = password
+        self.restmanUrl = restmanUrl
+        self.namespaces = namespaces
 
-    policyDetail = root.findall("l7:References/l7:Item/l7:Resource/l7:Policy/l7:PolicyDetail", namespaces)
-    policies = {}
-    for idx, detail in enumerate(policyDetail):
-        folderId = detail.attrib.get("folderId")
-        policyId = detail.attrib.get("id")
-        name = detail.find("l7:Name", namespaces).text
+    # Create directory structure
+    def createDirs(self, outputDir, bundleName):
+        confDir = outputDir + "/" + bundleName + "/" + self.gateway + "/conf/"
+        testDir = outputDir + "/" + bundleName + "/" + self.gateway + "/test/"
+        srcDir = outputDir + "/" + bundleName + "/" + self.gateway + "/src/"
+        docsDir = outputDir + "/" + bundleName + "/" + "/docs/"
 
-        details = [name, folderId]
-        policies[policyId] = details
+        pathlib.Path(confDir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(testDir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(srcDir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(docsDir).mkdir(parents=True, exist_ok=True)
 
-    return policies
+        return {"conf": confDir, "test": testDir, "src": srcDir, "doc": docsDir}
 
+    # FIXME: convert this method to use restman api
+    # Makes a list of the gmu browse command output
+    def browse(self):
+        gmuBrowse = os.popen("gmu browse --argFile " + self.argFile + " --recursive --showIds --hideProgress").read()
+        listing = list()
 
-# Get the folder id
-def getFolderId(username, password, restmanUrl, folderName):
-    foldersEndpoint = restmanUrl + "/1.0/folders?name=" + folderName
-    folder = requests.get(url=foldersEndpoint, auth=(username, password), verify=False)
-    return folder.text
+        for line in gmuBrowse.splitlines():
+            fields = line.split("\t")
+            if len(fields) == 3 and fields[2] != "Gateway REST Management Service":
+                item = {"type": fields[0].strip(), "id": fields[1].strip(), "path": fields[2].strip()}
+                listing.append(item)
+        return listing
 
+    # Gets the bundle
+    def getBundle(self, resourceType, id):
+        endpoint = self.restmanUrl + "/1.0/bundle"
+        parameters = {resourceType: id}
+        bundle = requests.get(url=endpoint, auth=(self.username, self.password), params=parameters, verify=False)
+        return bundle.text
 
-# Gets the bundle
-def getBundle(username, password, restmanUrl, resourceType, id):
-    endpoint = restmanUrl + "/1.0/bundle"
-    parameters = {resourceType: id}
-    bundle = requests.get(url=endpoint, auth=(username, password), params=parameters, verify=False)
-    return bundle.text
+    # Get the buildingblock policies
+    def getBuildingBlocks(self, bundle):
+        tree = ET.parse(bundle)
+        root = tree.getroot()
 
+        policyDetail = root.findall("l7:References/l7:Item/l7:Resource/l7:Policy/l7:PolicyDetail", self.namespaces)
+        policies = {}
+        for detail in policyDetail:
+            folderId = detail.attrib.get("folderId")
+            policyId = detail.attrib.get("id")
+            name = detail.find("l7:Name", self.namespaces).text
 
-# Adds mapping for building block policies for a given folder. This will create any folders if they are non existant
-def extendMapping(buildingBlockPolicies, xmlContents, namespaces):
-    tree = ET.parse(xmlContents)
-    root = tree.getroot()
-    bundle = ET.ElementTree()
+            details = [name, folderId]
+            policies[policyId] = details
 
-    mappings = root.findall("l7:Mappings/l7:Mapping", namespaces)
+        return policies
 
-    for mapping in mappings:
-        for prefix, url in namespaces.items():
-            ET.register_namespace(prefix, url)
-        srcId = mapping.get("srcId")
-        if(srcId in buildingBlockPolicies):
-            properties = ET.Element("l7:Properties")
-            propertiesMapBy = ET.SubElement(properties, "l7:Property")
-            stringValueMapBy = ET.SubElement(propertiesMapBy, "l7:StringValue")
-            propertiesMapBy.set("key", "MapBy")
-            stringValueMapBy.text = "path"
+    # Adds mapping for building block policies for a given folder. This will create any folders if they are non existant
+    def extendMapping(self, buildingBlockPolicies, bundle):
+        tree = ET.parse(bundle)
+        root = tree.getroot()
+        bundle = ET.ElementTree()
 
-            propertiesMapTo = ET.SubElement(properties, "l7:Property")
-            stringValueMapTo = ET.SubElement(propertiesMapTo, "l7:StringValue")
-            propertiesMapTo.set("key", "MapTo")
-            stringValueMapTo.text = "/buildingBlocks/" + buildingBlockPolicies[srcId][0]
-            mapping.insert(0, properties)
+        mappings = root.findall("l7:Mappings/l7:Mapping", self.namespaces)
 
-    bundle._setroot(root)
-    return bundle
+        for mapping in mappings:
+            for prefix, url in self.namespaces.items():
+                ET.register_namespace(prefix, url)
+            srcId = mapping.get("srcId")
+            if(srcId in buildingBlockPolicies):
+                properties = ET.Element("l7:Properties")
+                propertiesMapBy = ET.SubElement(properties, "l7:Property")
+                stringValueMapBy = ET.SubElement(propertiesMapBy, "l7:StringValue")
+                propertiesMapBy.set("key", "MapBy")
+                stringValueMapBy.text = "path"
 
+                propertiesMapTo = ET.SubElement(properties, "l7:Property")
+                stringValueMapTo = ET.SubElement(propertiesMapTo, "l7:StringValue")
+                propertiesMapTo.set("key", "MapTo")
+                stringValueMapTo.text = "/buildingBlocks/" + buildingBlockPolicies[srcId][0]
+                mapping.insert(0, properties)
 
-# def getServices(username, password, restmanUrl):
+        bundle._setroot(root)
+        return bundle
 
+    # Exports all the bundles for the given type
+    # FIXME: pretty print xml when writing out
+    def exportAll(self, exportList, outputDir, exportType="service"):
+        for item in exportList:
+            if item.get("type") == exportType:
+                # The name of the export type is the string following the last / character
+                name = item.get("path")[item.get("path").rindex("/") + 1:]
+                path = item.get("path")[:item.get("path").rindex("/")]
 
-namespaces = {"l7": "http://ns.l7tech.com/2010/04/gateway-management"}
-folders = ET.parse("dep.xml")
-root = folders.getroot()
+                # Create the directories to export to
+                dirs = self.createDirs(outputDir=outputDir, bundleName=name)
 
-refdict = {}
-references = root.findall("l7:Resource/l7:DependencyList/l7:Reference/l7:Dependencies/l7:Dependency", namespaces)
-for reference in references:
-    rId = reference.find("l7:Id", namespaces).text
-    rName = reference.find("l7:Name", namespaces).text
-    refdict[rId] = rName
+                # Get the bundle by calling the rest api
+                bundle = self.getBundle(exportType, item.get("id"))
 
-deps = root.findall("l7:Resource/l7:DependencyList/l7:Dependencies/l7:Dependency", namespaces)
-nested = root.findall("l7:Resource/l7:DependencyList/l7:Dependencies/l7:Dependency/l7:Dependencies/l7:Dependency", namespaces)
+                tree = ET.fromstring(bundle)
+                bundleXml = ET.ElementTree()
+                for prefix, url in self.namespaces.items():
+                    ET.register_namespace(prefix, url)
 
-# dependencies = root.findall("l7:Resource/l7:DependencyList/l7:Dependencies/l7:Dependency", namespaces)
-# references = root.findall("l7:Resource/l7:DependencyList/l7:Reference/l7:Dependencies/l7:Dependency", namespaces)
+                root = tree.find("l7:Resource/l7:Bundle", self.namespaces)
+                bundleXml._setroot(root)
+                bundleXml.write(dirs.get("src") + name + ".xml", encoding="utf-8", xml_declaration=True)
 
-# reflist = list()
-# for r in references:
-#     rname = r.find("l7:Name", namespaces).text
-#     rtype = r.find("l7:Type", namespaces).text
-#     rid = r.find("l7:Id", namespaces).text
-#     ref = {"id": rid, "type": rtype, "name": rname}
-#     reflist.append(ref)
+                # Create a mapping file with default action set to NewOrExisting
+                os.popen("gmu manageMappings --type " + exportType.upper() + " --action NewOrExisting --bundle " "\"" +
+                         dirs.get("src") + name + ".xml" + "\"" + " --outputFile " + "\"" + dirs.get("src") + name + "-mapping.xml" + "\"")
 
-# depList = list()
+                # Adjust mapping file for building block policies to map by name and path to building blocks folder
+                mappingAdjustedBundle = self.extendMapping(buildingBlockPolicies=self.getBuildingBlocks(dirs.get("src") + name + ".xml"), bundle=dirs.get("src") + name + ".xml")
+                mappingAdjustedBundle.write(dirs.get("src") + name + ".xml", encoding="utf-8", xml_declaration=True)
 
-# for d in dependencies:
-#     dname = d.find("l7:Name", namespaces).text
-#     dtype = d.find("l7:Type", namespaces).text
-#     did = d.find("l7:Id", namespaces).text
+                # Template the service
+                template = subprocess.Popen("gmu template --bundle " + "\"" + dirs.get("src") + "\"" + "/" + "\"" + name + "\"" + ".xml" + " --template " + "\"" + dirs.get("conf") + "\"" + "/" + "\"" + name + "\"" + ".properties", stdout=subprocess.PIPE, shell=True)
+                (output, err) = template.communicate()
+                template.wait()
 
-#     nested = d.findall("l7:Dependencies/l7:Dependency", namespaces)
-#     for n in nested:
-#         nid = n.find("l7:Id", namespaces).text
-#         ntype = n.find("l7:Type", namespaces).text
-#         nname = n.find("l7:Name", namespaces).text
-
-#         dep = {"id": did, "type": dtype, "name": dname, "depid": nid, "deptype": ntype, "depname": nname}
-#         depList.append(dep)
-
-
-# services = list()
-# for it in depList:
-#     # path=""
-#     fid=""
-#     if(it.get("deptype")=="SERVICE"):
-#         path = it.get("name") + "/" + it.get("depname")
-#         fid = it.get("id")
-#     for x in depList:
-#         if(x.get("depid")==fid):
-#             path = x.get("name") + "/" + path
-#             print(path)
-
-referenceList = list()
-references = root.findall("l7:Resource/l7:DependencyList/l7:Reference/l7:Dependencies/l7:Dependency", namespaces)
-for ref in references:
-    refId = ref.find("l7:Id", namespaces).text
-    refType = ref.find("l7:Type", namespaces).text
-    refName = ref.find("l7:Name", namespaces).text
-    ref = {"id": refId, "type": refType, "name": refName}
-    referenceList.append(ref)
-
-dependenciesList = list()
-dependencies = root.findall("l7:Resource/l7:DependencyList/l7:Dependencies/l7:Dependency", namespaces)
-for dep in dependencies:
-    depId = dep.find("l7:Id", namespaces).text
-    depType = dep.find("l7:Type", namespaces).text
-    depName = dep.find("l7:Name", namespaces).text
-
-    nestedDependencies = dep.findall("l7:Dependencies/l7:Dependency", namespaces)
-    for ndep in nestedDependencies:
-        ndepId = ndep.find("l7:Id", namespaces).text
-        ndepType = ndep.find("l7:Type", namespaces).text
-        ndepName = ndep.find("l7:Name", namespaces).text
-        dep = {"depId": depId, "depType": depType, "depName": depName, "nestedId": ndepId, "nestedType": ndepType, "nestedName": ndepName}
-        dependenciesList.append(dep)
+                # Add folder path property
+                properties = open(dirs.get("conf") + name + ".properties", "a")
+                properties.write("service.folderpath=/" + path)
+                properties.close()
 
 
-for i, nItem in enumerate(dependenciesList):
-    if(nItem.get("nestedType") == "SERVICE"):
-        # path = nItem.get("depName") + "/" + nItem.get("nestedName")
+if __name__ == "__main__":
+    argfile = "/home/amresh/Projects/ziggo/layer7/gitlab.com/layer7-cicd/workspace/dev-argFile.properties"
+    restmanurl = "https://gateway-dev:8443/restman"
+    username = "admin"
+    password = "password"
+    namespaces = {"l7": "http://ns.l7tech.com/2010/04/gateway-management"}
+    gateway = "NL_BSS"
+    gw = Gateway(argFile=argfile, username=username, password=password, restmanUrl=restmanurl, namespaces=namespaces, gateway=gateway)
 
-        dependencyFound = True
-        while(dependencyFound):
-            for item in dependenciesList:
-                itemDepId = nItem.get("depId")
-                itemNestedId = item.get("nestedId")
-                if(itemDepId == itemNestedId):
-                    s = item.get("depName") + "/" + nItem.get("depName")
-                    print(s)
-                else:
-                    dependencyFound = False
+    gw.exportAll(exportList=gw.browse(), outputDir="/home/amresh/Projects/ziggo/layer7/gitlab.com/bsl")
+
+
+# # Get the folder id
+# def getFolderId(username, password, restmanUrl, folderName):
+#     foldersEndpoint = restmanUrl + "/1.0/folders?name=" + folderName
+#     folder = requests.get(url=foldersEndpoint, auth=(username, password), verify=False)
+#     return folder.text
+
+
+# def exportAll(listing, exportType, outputDir, gateway, username, password, restmanUrl, namespaces):
+#     for item in listing:
+#         if item.get("type") == exportType:
+#             # The name of the export type is the string following the last / character
+#             name = item.get("path")[item.get("path").rindex("/") + 1:]
+#             path = item.get("path")[:item.get("path").rindex("/")]
+
+#             # Create the directories to export to
+#             confDir = outputDir + "/" + name + "/" + gateway + "/conf/"
+#             testDir = outputDir + "/" + name + "/" + gateway + "/test/"
+#             srcDir = outputDir + "/" + name + "/" + gateway + "/src/"
+#             docsDir = outputDir + "/" + name + "/" + "/docs/"
+
+#             pathlib.Path(confDir).mkdir(parents=True, exist_ok=True)
+#             pathlib.Path(testDir).mkdir(parents=True, exist_ok=True)
+#             pathlib.Path(srcDir).mkdir(parents=True, exist_ok=True)
+#             pathlib.Path(docsDir).mkdir(parents=True, exist_ok=True)
+
+#             # Get the bundle by calling the rest api
+#             endpoint = restmanUrl + "/1.0/bundle"
+#             parameters = {item.get("type"): item.get("id")}
+#             bundle = requests.get(url=endpoint, auth=(username, password), params=parameters, verify=False)
+
+#             tree = ET.fromstring(bundle.text)
+#             bundleXml = ET.ElementTree()
+#             for prefix, url in namespaces.items():
+#                 ET.register_namespace(prefix, url)
+
+#             root = tree.find("l7:Resource/l7:Bundle", namespaces)
+#             bundleXml._setroot(root)
+#             bundleXml.write(srcDir + name + ".xml", encoding="utf-8", xml_declaration=True)
+
+#             # Create a mapping file with default action set to NewOrExisting
+#             os.popen("gmu manageMappings --type " + exportType.upper() + " --action NewOrExisting --bundle " "\"" +
+#                      srcDir + name + ".xml" + "\"" + " --outputFile " + "\"" + srcDir + name + "-mapping.xml" + "\"")
+
+#             # Add mapping to map to building blocks folder
+#             # buildingBlocks = getBuildingBlocks(bundleXml,namespaces,)
+
+
+# folderid=getFolderId("admin","password","https://gateway-tst:8443/restman","buildingBlocks")
+# print(folderid)
+# # e = exportAll(browse("../workspace/tst-argFile.properties"), "service", "/home/amresh/Projects/ziggo/layer7/gitlab.com/bsl", "NL_BSS",
+# #               "admin", "password", "https://gateway-tst:8443/restman", {"l7": "http://ns.l7tech.com/2010/04/gateway-management"})
+
+# # bundle = getBundle("admin","password","https://gateway-tst:8443/restman","service","e73aa375c92bf77a0b52a366672b0306")
+# # # tree = ET.parse(bundle)
+# # root=ET.fromstring(bundle)
+
+# # ns = {"l7":"http://ns.l7tech.com/2010/04/gateway-management"}
+# # root = root.find("l7:Resource/l7:Bundle",ns)
+
+# # bundle=ET.ElementTree()
+# # ET.register_namespace("l7", "http://ns.l7tech.com/2010/04/gateway-management")
+# # bundle._setroot(root)
+# # bundle.write("out.xml",encoding="utf-8",xml_declaration=True)
+
+# # browse = browse("../workspace/tst-argFile.properties")
+# # for it in browse:
+# #     print(it)
